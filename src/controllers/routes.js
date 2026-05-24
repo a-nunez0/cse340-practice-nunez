cf798a723dc5387228c2e6f7068270f4d4086ac0
a-nunez0 <122509183+a-nunez0@users.noreply.github.com>
2026-05-23 19:41:38 -0600

    rest

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const facultyController = require('./faculty/faculty');
+
 router.get('/', (req, res) => {
     const title = 'Welcome Home';
     res.render('home', { title });
@@ -15,5 +17,10 @@ router.get('/products', (req, res) => {
     const title = 'Our Products';
     res.render('products', { title });
 });
+
+// Faculty routes
+router.get('/faculty', facultyController.facultyListPage);
+
+router.get('/faculty/:facultyId', facultyController.facultyDetailPage);
 
 module.exports = router;
